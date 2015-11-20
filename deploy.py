34fdff7494e165c66ecf66d1fbf3fc156f8338b0
tianhaowei <tianhaowei@qingtingfm.com>
2015-11-20 16:54:23 +0800

    fix

--- deploy.py
+++ deploy.py
@@ -24,11 +24,9 @@ class ChDir:
 
 
 def main():
-    os.system('hugo --theme=hyde --baseUrl="http://luosha865.github.io/')
+    os.system('hugo --theme=hyde --baseUrl="http://luosha865.github.io/"')
     with ChDir("public"):
-        os.system('git init')
-        os.system('git remote add origin git@github.com:luosha865/luosha865.github.io.git')
-        os.system('git add -A')
+        os.system('git add --all')
         os.system('git commit -m "commit"')
         os.system('git push -u origin master')
 
